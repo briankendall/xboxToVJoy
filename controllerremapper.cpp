c32230096f924453f7fc5602fdfec1aa1c9bcb9e
Brian Kendall <guygizmo@gmail.com>
2014-08-08 17:06:54 -0400

    Migrated in remapping code

--- controllerremapper.cpp
+++ controllerremapper.cpp
@@ -10,9 +10,187 @@ XInputGetStateEx_t XInputGetStateEx = NULL;
 
 #define XINPUT_GAMEPAD_GUIDE 0x400
 
+const int buttonFlags[kButtonCount] = {XINPUT_GAMEPAD_A, XINPUT_GAMEPAD_B, XINPUT_GAMEPAD_X, XINPUT_GAMEPAD_Y,
+                                       XINPUT_GAMEPAD_LEFT_SHOULDER, XINPUT_GAMEPAD_RIGHT_SHOULDER, XINPUT_GAMEPAD_BACK,
+                                       XINPUT_GAMEPAD_START, XINPUT_GAMEPAD_LEFT_THUMB, XINPUT_GAMEPAD_RIGHT_THUMB,
+                                       XINPUT_GAMEPAD_GUIDE};
+const int directionFlags[4] = {XINPUT_GAMEPAD_DPAD_LEFT, XINPUT_GAMEPAD_DPAD_UP, XINPUT_GAMEPAD_DPAD_RIGHT,
+                               XINPUT_GAMEPAD_DPAD_DOWN};
+
+long clamp(long val, long min, long max)
+{
+	if (val < min) {
+		val = min;
+	}
+	if (val > max) {
+		val = max;
+	}
+	return val;
+}
+
+long xboxAxisToVJoy(SHORT val, bool reverse)
+{
+	long n = long(val) / 2 + 16384;
+
+	if (reverse) {
+		n = 32767 - n;
+	}
+
+	return clamp(n + 1, 1, 32768);
+}
+
+long xboxTriggerToVJoy(BYTE val)
+{
+    return long((double(val) / 255.0) * 32767.0) + 1;
+}
+
+bool directionPressed(const QVector<bool> &buttons, bool left, bool up, bool right, bool down)
+{
+	return buttons[0] == left && buttons[1] == up && buttons[2] == right && buttons[3] == down;
+}
+
+void Controller::initialize()
+{
+    buttonsDown.fill(false, kButtonCount);
+    directionDown.fill(false, 4);
+    lastLeftTrigger = kAxisUnset;
+    lastRightTrigger = kAxisUnset;
+    lastLX = kAxisUnset;
+    lastLY = kAxisUnset;
+    lastRX = kAxisUnset;
+    lastRY = kAxisUnset;
+	lastPacketNumber = 0;
+	handledFirstPacket = false;
+    lastConnected = false;
+}
+
+void Controller::doControllerMap()
+{
+    UINT deviceId = deviceIndex + 1;
+    XINPUT_STATE controllerState;
+    ZeroMemory(&controllerState, sizeof(XINPUT_STATE));
+    DWORD result = XInputGetStateEx(deviceIndex, &controllerState);
+    bool connected = (result == ERROR_SUCCESS);
+
+    if (!connected) {
+        if (lastConnected) {
+            reset();
+        }
+        lastConnected = connected;
+        return;
+    }
+    
+    if (lastPacketNumber == controllerState.dwPacketNumber && handledFirstPacket) {
+        // Xbox controller state hasn't changed, no need to do anything
+        return;
+    }
+    
+    const XINPUT_GAMEPAD &state = controllerState.Gamepad;
+    handledFirstPacket = true;
+    lastPacketNumber = controllerState.dwPacketNumber;
+
+    for (int i = 0; i < kButtonCount; ++i) {
+        bool playerButtonDown = (state.wButtons & buttonFlags[i]) != 0;
+
+        if (playerButtonDown && !buttonsDown[i]) {
+            SetBtn(true, deviceId, i+1);
+
+        } else if (!playerButtonDown && buttonsDown[i]) {
+            SetBtn(false, deviceId, i+1);
+
+        }
+
+        buttonsDown[i] = playerButtonDown;
+    }
+
+    bool directionChanged = false;
+    for (int i = 0; i < 4; ++i) {
+        bool playerButtonDown = (state.wButtons & directionFlags[i]) != 0;
+        
+        if (directionDown[i] != playerButtonDown) {
+            directionChanged = true;
+            directionDown[i] = playerButtonDown;
+        }
+    }
+
+    if (directionChanged) {
+        if (directionPressed(directionDown, true, false, false, false)) {
+            SetContPov(27000, deviceId, 1);
+        } else if (directionPressed(directionDown, true, true, false, false)) {
+            SetContPov(31500, deviceId, 1);
+        } else if (directionPressed(directionDown, false, true, false, false)) {
+            SetContPov(0, deviceId, 1);
+        } else if (directionPressed(directionDown, false, true, true, false)) {
+            SetContPov(4500, deviceId, 1);
+        } else if (directionPressed(directionDown, false, false, true, false)) {
+            SetContPov(9000, deviceId, 1);
+        } else if (directionPressed(directionDown, false, false, true, true)) {
+            SetContPov(13500, deviceId, 1);
+        } else if (directionPressed(directionDown, false, false, false, true)) {
+            SetContPov(18000, deviceId, 1);
+        } else if (directionPressed(directionDown, true, false, false, true)) {
+            SetContPov(22500, deviceId, 1);
+        } else {
+            SetContPov(-1, deviceId, 1);
+        }
+    }
+
+    if (state.bLeftTrigger != lastLeftTrigger) {
+        SetAxis(xboxTriggerToVJoy(state.bLeftTrigger), deviceId, HID_USAGE_SL0);
+        lastLeftTrigger = state.bLeftTrigger;
+    }
+
+    if (state.bRightTrigger != lastRightTrigger) {
+        SetAxis(xboxTriggerToVJoy(state.bRightTrigger), deviceId, HID_USAGE_SL1);
+        lastRightTrigger = state.bRightTrigger;
+    }
+
+    if (state.sThumbLX != lastLX) {
+        SetAxis(xboxAxisToVJoy(state.sThumbLX, false), deviceId, HID_USAGE_X);
+        lastLX = state.sThumbLX;
+    }
+
+    if (state.sThumbLY != lastLY) {
+        SetAxis(xboxAxisToVJoy(state.sThumbLY, true), deviceId, HID_USAGE_Y);
+        lastLY = state.sThumbLY;
+    }
+
+    if (state.sThumbRX != lastRX) {
+        SetAxis(xboxAxisToVJoy(state.sThumbRX, false), deviceId, HID_USAGE_RX);
+        lastRX = state.sThumbRX;
+    }
+    
+    if (state.sThumbRY != lastRY) {
+        SetAxis(xboxAxisToVJoy(state.sThumbRY, true), deviceId, HID_USAGE_RY);
+        lastRY = state.sThumbRY;
+    }
+}
+
+void Controller::reset()
+{
+    UINT deviceId = deviceIndex + 1;
+    
+    initialize();
+    
+    ResetVJD(deviceId);
+    SetContPov(-1, deviceId, 1);
+    SetAxis(1, deviceId, HID_USAGE_SL0);
+    SetAxis(1, deviceId, HID_USAGE_SL1);
+    SetAxis(0x4000, deviceId, HID_USAGE_X);
+    SetAxis(0x4000, deviceId, HID_USAGE_Y);
+    SetAxis(0x4000, deviceId, HID_USAGE_RY);
+    SetAxis(0x4000, deviceId, HID_USAGE_RX);
+}
+
 ControllerRemapper::ControllerRemapper(QObject *parent) :
     QThread(parent)
 {
+}
+
+void ControllerRemapper::throwInitError(QString msg)
+{
+    emit initializationError(msg);
+    exit(1);
 }
 
 bool ControllerRemapper::checkAxisExists(UINT deviceId, UINT axis, QString axisName)
@@ -76,9 +254,10 @@ void ControllerRemapper::initializeDevice(UINT deviceId)
     
 	// Get the number of buttons and POV Hat switchessupported by this vJoy device
     
-    if (GetVJDButtonNumber(deviceId) < 11) {
-        throwInitError(QString("vJoy Device %1 does not have at least 11 buttons.\nPlease make sure "
-                               "the first four vJoy devices all have 11 buttons or more before launching.").arg(deviceId));
+    if (GetVJDButtonNumber(deviceId) < kButtonCount) {
+        throwInitError(QString("vJoy Device %1 does not have at least %2 buttons.\nPlease make sure "
+                               "the first four vJoy devices all have %3 buttons or more before "
+                               "launching.").arg(deviceId).arg(kButtonCount).arg(kButtonCount));
         return;
     }
     
@@ -101,8 +280,11 @@ void ControllerRemapper::initializeDevice(UINT deviceId)
 
 void ControllerRemapper::initialize()
 {
-    for(UINT deviceId = 1; deviceId <= 4; ++deviceId) {
-        initializeDevice(deviceId);
+    for(UINT index = 0; index < 4; ++index) {
+        initializeDevice(index+1);
+        controllers[index].deviceIndex = index;
+        controllers[index].initialize();
+        controllers[index].reset();
     }
     
     // Get access to XInputGetStateEx so we can query the state of the Guide/Home button:
@@ -125,21 +307,32 @@ void ControllerRemapper::deinitialize()
     }
 }
 
-void ControllerRemapper::throwInitError(QString msg)
+void ControllerRemapper::poll()
 {
-    emit initializationError(msg);
-    exit(1);
+    for(UINT index = 0; index < 4; ++index) {
+       controllers[index].doControllerMap(); 
+    }
 }
 
 void ControllerRemapper::run()
 {
-    qDebug() << "Starting thread";
+    qDebug() << "Starting remap thread";
     
     initialize();
     
+    pollTimer = new QTimer();
+    pollTimer->moveToThread(this);
+    connect(pollTimer, SIGNAL(timeout()), this, SLOT(poll()), Qt::DirectConnection);
+    pollTimer->setTimerType(Qt::PreciseTimer);
+    pollTimer->start(1000 / kPollingCyclesPerSecond);
+    
     exec();
     
-    qDebug() << "Thread deinitializing...";
+    qDebug() << "Remap thread deinitializing...";
+    
+    pollTimer->stop();
+    delete pollTimer;
     deinitialize();
-    qDebug() << "Thread finished";
+    
+    qDebug() << "Remap thread finished";
 }
--- controllerremapper.h
+++ controllerremapper.h
@@ -4,6 +4,31 @@
 #include <windows.h>
 #include <QThread>
 #include <QSet>
+#include <QVector>
+#include <QTimer>
+
+#define kButtonCount 11
+// The following is a value that an XInput axis is guaranteed never to have, and
+// is therefore safe to use as a flag indicating an unset value:
+#define kAxisUnset -100000
+#define kPollingCyclesPerSecond 200
+
+class Controller {
+public:
+    Controller() {};
+    void initialize();
+    void doControllerMap();
+    void reset();
+    
+    UINT deviceIndex;
+private:
+    QVector<bool> buttonsDown;
+    QVector<bool> directionDown;
+    long lastLeftTrigger, lastRightTrigger, lastLX, lastLY, lastRX, lastRY;
+    DWORD lastPacketNumber;
+    bool lastConnected;
+    bool handledFirstPacket;
+};
 
 class ControllerRemapper : public QThread
 {
@@ -16,6 +41,7 @@ signals:
     void initializationError(QString msg);
     
 public slots:
+    void poll();
     
 protected:
     virtual void run() Q_DECL_OVERRIDE;
@@ -26,6 +52,9 @@ protected:
     bool checkAxisExists(UINT deviceId, UINT axis, QString axisName);
     
     QSet<UINT> initializedDevices;
+    Controller controllers[4];
+    QTimer *pollTimer;
+    
 };
 
 #endif // CONTROLLERREMAPPER_H
